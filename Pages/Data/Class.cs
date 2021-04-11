da5d311ba6613984e0f6fd137e568ffc872b22b3
NotSoSaneExile <sciemastyle@wp.pl>
2021-04-11 19:56:03 +0200

    2. Z właściwości bazy danych pobierz wartość ConnectionString i zapisz ją w appsettings.json w kluczu EFDemoDB:

---
+++ Pages/Data/Class.cs
@@ -0,0 +1,10 @@
+﻿using FizzBuzzWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FizzBuzzWeb.Pages.Data
+{
+    public class FizzBuzzContext: DbContext {
+        public DbSet<FizzBuzz> FizzBuzz { get; set; }
+
+    }
+}
